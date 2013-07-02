a8b00584e150c26aacfed91a24150f4bf24426bf
Christian Simon <cs@f03.eu>
2013-07-02 08:19:17 +0200

    [TASK] Add imagemagick

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure packages
-apt-get install -y latex-beamer latex-xcolor pgf texlive-pstricks texlive-generic-recommended lacheck biblatex make
+apt-get install -y latex-beamer latex-xcolor pgf texlive-pstricks texlive-generic-recommended lacheck biblatex make imagemagick
 
 # change to Latex dir
 cd $WORKSPACE/source/
