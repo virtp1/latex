53a127101194729f684834e8ca82d3dbb2ea107b
Christian Simon <cs@f03.eu>
2013-07-02 08:20:39 +0200

    [TASK] build.sh correction

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure packages
-apt-get install -y latex-beamer latex-xcolor pgf texlive-pstricks texlive-generic-recommended lacheck biblatex make imagemagick
+apt-get install -y latex-beamer latex-xcolor pgf texlive-pstricks texlive-generic-recommended lacheck biblatex make imagemagick ghostscript
 
 # change to Latex dir
 cd $WORKSPACE/source/
