61c6d58dc4f8e5bf5fc3bbd74edff3a181c5d15f
Christian Simon <cs@f03.eu>
2013-07-02 08:17:44 +0200

    [TASK] Jenkins test

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ensure packages
+apt-get install -y latex-beamer latex-xcolor pgf texlive-pstricks texlive-generic-recommended lacheck biblatex make
+
+# change to Latex dir
+cd $WORKSPACE/source/
+
+# Run makefile
+make 
